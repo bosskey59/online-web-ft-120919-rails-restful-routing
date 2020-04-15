03d27da2a534ff01797e86557fb1ab370dc9803d
Alejandro Aguilar <aaguilar2@hou-us05186-mbp13.attlocal.net>
2020-04-15 14:07:55 -0500

    got to new but not create

--- app/controllers/sushis_controller.rb
+++ app/controllers/sushis_controller.rb
@@ -1,4 +1,23 @@
 class SushisController < ApplicationController
 
+    def index
+        @sushis = Sushi.all
+    end
 
+    def show
+        @sushi = Sushi.find_by(id:params[:id])
+        if @sushi
+            render "show"
+        else
+            redirect_to sushis_path
+        end
+    end
+
+    def new
+
+    end
+
+    def create
+        byebug
+    end
 end
